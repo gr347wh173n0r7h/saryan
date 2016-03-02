391c9a47e5e0f2de582adfa7fbb2caa8879f278b
gr347wh173n0r7h <jordan.a.petersen@gmail.com>
2016-03-01 19:00:07 -0800

    User accound UA protection writen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,27 @@
 class UsersController < ApplicationController
 	def show
 		# WRITE PROTECT AFTER DEV!!!
-		@user = User.find(params[:id])
+		if session[:user_id].to_s == params[:id].to_s
+			@user = User.find(params[:id])
+		else
+			redirect_to current_user
+		end
 	end
 
-  	def new
-			if  logged_in?
-				redirect_to root_path
-			elsif
-  			@user = User.new
-			end
-  	end
+	def new
+		if  logged_in?
+			redirect_to root_path
+		elsif
+			@user = User.new
+		end
+	end
 
-  	def create
-  		@user = User.new(user_params)
-  		if @user.save
-        log_in @user
-  			flash[:success] = "Welcome to the SARYAN"
-  			redirect_to @user
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			log_in @user
+			flash[:success] = "Welcome to the SARYAN"
+			redirect_to @user
 		else
 			flash[:failure] = "Error occured"
 			render 'new'
