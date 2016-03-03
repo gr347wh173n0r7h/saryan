ec08949c7f9b4c2f88ffe93a9dd4e3893904674a
SySarmad <syed.sarmad15@gmail.com>
2016-03-02 16:40:57 -0800

    Fixed courses migrate

--- db/migrate/20160302225055_create_courses.rb
+++ db/migrate/20160302225055_create_courses.rb
@@ -3,12 +3,7 @@ class CreateCourses < ActiveRecord::Migration
     create_table :courses do |t|
       t.string :dep
       t.string :name
-<<<<<<< HEAD:db/migrate/20151109021044_create_courses.rb
-      t.text :desc
-      t.integer :course_num
-=======
       t.string :abbrev
->>>>>>> newDB2:db/migrate/20160302225055_create_courses.rb
       t.integer :units
       t.text :desc
 
