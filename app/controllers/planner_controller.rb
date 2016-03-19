64e5a139f6c95b47fa94beac7ed0f7e09c8b280d
gr347wh173n0r7h <jordan.a.petersen@gmail.com>
2016-03-19 00:07:37 -0700

    Planner loggin protection fix

--- app/controllers/planner_controller.rb
+++ app/controllers/planner_controller.rb
@@ -3,21 +3,21 @@ class PlannerController < ApplicationController
 
     if  !logged_in?
       redirect_to login_path
-    end
-    @academic = Academic.where(:user_id => current_user.id)
-    if @academic.empty?
-      redirect_to academics_new_path
     else
-      @academic = @academic.take
-      @school = School.find(@academic.school_id)
-      @major = Major.find(@academic.major_id)
-      # @dep = School.find(@academic.department_id)
-      @catalog = Catalog.find(@academic.catalog_id)
-      @courses = @catalog.courses
-      @cat = @catalog.categories.all
-      @sub_cat = SubCategory.all
+      @academic = Academic.where(:user_id => current_user.id)
+      if @academic.empty?
+        redirect_to academics_new_path
+      else
+        @academic = @academic.take
+        @school = School.find(@academic.school_id)
+        @major = Major.find(@academic.major_id)
+        # @dep = School.find(@academic.department_id)
+        @catalog = Catalog.find(@academic.catalog_id)
+        @courses = @catalog.courses
+        @cat = @catalog.categories.all
+        @sub_cat = SubCategory.all
+      end
     end
-
   end
 
   def new
