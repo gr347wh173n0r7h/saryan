e47e9588b4082b5e5120df144c78e043b94c406f
SySarmad <syed.sarmad15@gmail.com>
2016-04-04 15:26:07 -0700

    Seed file with Plan!

--- db/migrate/20160401050030_create_saved_plans.rb
+++ db/migrate/20160401050030_create_saved_plans.rb
@@ -2,6 +2,7 @@ class CreateSavedPlans < ActiveRecord::Migration
   def change
     create_table :saved_plans do |t|
       t.string :name
+      t.string :major
       t.timestamps null: false
     end
   end
