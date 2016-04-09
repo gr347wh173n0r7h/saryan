e3c73e952849d41f95f03138e65474a1dc3dd655
gr347wh173n0r7h <jordan.a.petersen@gmail.com>
2016-04-09 11:21:59 -0700

    Planner now on New method, and Footer link fixes

--- app/controllers/planner_controller.rb
+++ app/controllers/planner_controller.rb
@@ -1,6 +1,5 @@
 class PlannerController < ApplicationController
   def index
-
     if  !logged_in?
       redirect_to login_path
     else
@@ -11,12 +10,8 @@ class PlannerController < ApplicationController
         @academic = @academic.take
         @school = School.find(@academic.school_id)
         @major = Major.find(@academic.major_id)
-        # @dep = School.find(@academic.department_id)
         @catalog = Catalog.find(@academic.catalog_id)
         @courses = @catalog.courses
-        # @sup_cat = @catalog.super_cats
-        # @cat = @sup_cat.categories
-        # @sub_cat = @cat.sub_categories
       end
     end
   end
@@ -24,6 +19,35 @@ class PlannerController < ApplicationController
   def new
     if  !logged_in?
       redirect_to login_path
+    else
+      @academic = Academic.where(:user_id => current_user.id)
+      if @academic.empty?
+        redirect_to academics_new_path
+      else
+        @academic = @academic.take
+        @school = School.find(@academic.school_id)
+        @major = Major.find(@academic.major_id)
+        @catalog = Catalog.find(@academic.catalog_id)
+        @courses = @catalog.courses
+      end
     end
   end
+
+  def show
+    if  !logged_in?
+      redirect_to login_path
+    else
+      @academic = Academic.where(:user_id => current_user.id)
+      if @academic.empty?
+        redirect_to academics_new_path
+      else
+        @academic = @academic.take
+        @school = School.find(@academic.school_id)
+        @major = Major.find(@academic.major_id)
+        @catalog = Catalog.find(@academic.catalog_id)
+        @courses = @catalog.courses
+      end
+    end
+  end
+
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,8 @@ class SessionsController < ApplicationController
     	if user && user.authenticate(params[:session][:password])
     		log_in user
 	      	params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-    		redirect_to user
+    		# redirect_to user
+				redirect_to planner_new_path
 		else
 			flash.now[:danger] = 'Invalid email or password'
 			render 'new'
