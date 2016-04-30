2abf951822b23ad1035f9e130ad6434ba959af0a
gr347wh173n0r7h <jordan.a.petersen@gmail.com>
2016-04-29 22:39:14 -0700

    Edit Planner fixes and errors checks, a few bugs

--- app/controllers/planner_controller.rb
+++ app/controllers/planner_controller.rb
@@ -72,33 +72,37 @@ class PlannerController < ApplicationController
     if  !logged_in?
       redirect_to login_path
     else
-      @academic = Academic.where(:user_id => current_user.id)
-      if @academic.empty?
-        redirect_to academics_new_path
-      else
-        @academic = @academic.take
-        @school = School.find(@academic.school_id)
-        @major = Major.find(@academic.major_id)
-        @catalog = Catalog.find(@academic.catalog_id)
-        @courses = @catalog.courses
-
-        # @plans = Planner.where(:user_id => current_user.id).all
-        @user = current_user
-        @plans = @user.planners.all
-        @saved_plans = SavedPlan.where(:major => @major.name).all
-
-        @plan = Planner.find(params[:id])
-        if @plan[:user_id] == @user[:id]
-          @semester = @plan.semesters
-          @sem_courses = []
-          @semester.each do |sem|
-            sem.courses.each do |c|
-              @sem_courses.push c
-            end
-          end
+      if(params[:id] != 0)
+        @academic = Academic.where(:user_id => current_user.id)
+        if @academic.empty?
+          redirect_to academics_new_path
         else
-          redirect_to planner_path
+          @academic = @academic.take
+          @school = School.find(@academic.school_id)
+          @major = Major.find(@academic.major_id)
+          @catalog = Catalog.find(@academic.catalog_id)
+          @courses = @catalog.courses
+
+          # @plans = Planner.where(:user_id => current_user.id).all
+          @user = current_user
+          @plans = @user.planners.all
+          @saved_plans = SavedPlan.where(:major => @major.name).all
+
+          @plan = Planner.find(params[:id])
+          if @plan[:user_id] == @user[:id]
+            @semester = @plan.semesters
+            @sem_courses = []
+            @semester.each do |sem|
+              sem.courses.each do |c|
+                @sem_courses.push c
+              end
+            end
+          else
+            redirect_to planner_path
+          end
         end
+      else
+        redirect_to planner_path
       end
     end
   end
@@ -111,7 +115,7 @@ class PlannerController < ApplicationController
     planner_params[:plan_string].split(',').each do |p|
       ident = p.split('-')
       if ident[0] == 'S'
-        @sem = @planner.semesters.build(name: ident[1])
+        @sem = @planner.semesters.build(name: ident[1], year: ident[1][-4..-1].to_)
         @sem.save
       else
         @course = Course.find(ident[1])
