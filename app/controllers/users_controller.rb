4702be3b20d4cd151502264e920d288b820a38c5
Lisa <delphond@yahoo.com>
2016-05-16 11:50:18 -0700

    account stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,19 @@ class UsersController < ApplicationController
 	before_action :logged_in_user, only: [:show]
 	before_action :not_logged_in_user, only: [:new]
 
+	helper_method :user_status
+
 	def show
-			if session[:user_id].to_s == params[:id].to_s
-				@user = User.find(params[:id])
-				# @academic = Academic.where(user_id: params[:id])
-			else
-				redirect_to current_user
-			end
+		if session[:user_id].to_s == params[:id].to_s
+			@user = User.find(params[:id])
+			# testing lines up to else.
+			@academic = Academic.where(user_id: params[:id])
+			@academic = @academic.take
+			@major = Major.find(@academic.major_id)
+			user_status
+		else
+			redirect_to current_user
+		end
 	end
 
 	def new
@@ -24,6 +30,41 @@ class UsersController < ApplicationController
 		else
 			flash[:failure] = "Error occured"
 			render 'new'
+		end
+	end
+
+	def edit
+		if session[:user_id].to_s == params[:id].to_s
+			@user = User.find(params[:id])
+			render action: "edit"
+		else
+			redirect_to current_user
+		end
+  end
+
+  def update
+   #   sign_in @user
+    @user = User.find_by_id(params[:id])
+		if @user.update_attributes(params[:user])
+   # @user.update_attributes(params[:user])
+
+		flash[:success] = "Profile updated"
+	#	redirect_to current_user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_path
+  end
+
+	def user_status
+		if @user.status == 'S'
+			$user_status = "Student"
+		else $user_status = "Advisor"
 		end
 	end
 
